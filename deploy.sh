bc71ffa6a487b3e152ae35091888632e2a06a04b
Andrew Heiss <andrew@andrewheiss.com>
2024-01-29 11:23:06 -0500

    Use new server

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-REMOTE_HOST="cloud"
-REMOTE_DIR="~/sites/andrewheiss.com/public_html"
+REMOTE_HOST="ath-cloud"
+REMOTE_DIR="~/sites/andrewheiss.com/public"
 REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
 
 echo "Uploading new changes to remote server..."
